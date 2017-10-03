79fc82a286c55c9e36620190c5ed295441bc0311
vyacheslavmartynenko <evilacer5755g@gmail.com>
2017-10-03 23:54:46 +0300

    fix request

--- src/main/kotlin/main/Main.kt
+++ src/main/kotlin/main/Main.kt
@@ -4,16 +4,22 @@ import networking.ApiCall
 import java.util.concurrent.Executors
 
 object Main {
-    val executorService = Executors.newFixedThreadPool(1)
-    val apiCall = ApiCall.Factory.create()
+    private val executorService = Executors.newFixedThreadPool(1)
+    private val apiCall = ApiCall.Factory.create()
 
     @JvmStatic
     fun main(args: Array<String>) {
-        executorService.execute(getRunnable(1226926872))
+        executorService.execute(getApplicationInfo(1226926872))
         executorService.shutdown()
     }
 
-    fun getRunnable(id: Int): Runnable {
-        return Runnable { apiCall.getApplicationInfo(id).subscribe { it.results.forEach { print(it.description) } } }
+    private fun getApplicationInfo(id: Int): Runnable {
+        return Runnable {
+            apiCall.getApplicationInfo(id).subscribe {
+                it.results.forEach {
+                    print(it.description + " " + it.userRating)
+                }
+            }
+        }
     }
 }
--- src/main/kotlin/networking/model/ApplicationInfoResponse.kt
+++ src/main/kotlin/networking/model/ApplicationInfoResponse.kt
@@ -4,5 +4,9 @@ import com.google.gson.annotations.SerializedName
 
 data class ApplicationInfoResponse(@SerializedName("resultCount") val resultCount: String,
                                    @SerializedName("results") val results: List<Result>) {
-    data class Result(@SerializedName("description") val description: String)
+    data class Result(@SerializedName("trackId") val trackId: Int,
+                      @SerializedName("trackName") val trackName: String,
+                      @SerializedName("bundleId") val bundleId: String,
+                      @SerializedName("description") val description: String,
+                      @SerializedName("averageUserRatingForCurrentVersion") val userRating: Double)
 }
