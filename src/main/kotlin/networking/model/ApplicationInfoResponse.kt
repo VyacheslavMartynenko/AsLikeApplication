3f4afe4477ad6b8081818e36cbfbc3d468f41347
vyacheslavmartynenko <evilacer5755g@gmail.com>
2017-10-04 22:14:49 +0300

    fix request

--- src/main/kotlin/networking/model/ApplicationInfoResponse.kt
+++ src/main/kotlin/networking/model/ApplicationInfoResponse.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 data class ApplicationInfoResponse(@SerializedName("resultCount") val resultCount: String,
                                    @SerializedName("results") val results: List<Result>) {
-    public data class Result(@SerializedName("trackId") val trackId: Int,
+    data class Result(@SerializedName("trackId") val trackId: Int,
                       @SerializedName("trackName") val trackName: String,
                       @SerializedName("bundleId") val bundleId: String,
                       @SerializedName("description") val description: String,
